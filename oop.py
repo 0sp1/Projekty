50b2f3841f234c9b194e7b609c648c3d66831d5b
unknown <patrykszuraa@gmail.com>
2025-05-30 20:19:21 +0200

    commit to what ?

--- oop.py
+++ oop.py
@@ -1,8 +1,28 @@
-class Dog:
-    def __init__(self, name):
-        self.name = name
-    def bark(self):
-        print(f"Retard {self.name}")
+# class Dog:
+#     def __init__(self, name, age):
+#         self.age = age
+#         self.name = name
+    
+#     def get_name(self):
+#         return self.name
+    
+#     def get_age(self):
+#         return self.age
 
-dog1 = Dog("Ryszard")
-dog1.bark()
+#     def add_one(self, x):
+#         return x + 1
+    
+#     def set_age(self, age):
+#         self.age = age
+
+#     def bark(self):
+#         print("bark")
+
+# #print(d.get_name())
+# d = Dog("Robert", 12)
+# d.set_age(5)
+# print(d.get_age())
+
+# # d2 = Dog("Andrzej", 4)
+# # print(d2.get_age())
+
