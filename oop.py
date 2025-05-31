cbe3216727ecd2448b9b05eaa5b015454fcc62a1
unknown <patrykszuraa@gmail.com>
2025-05-31 19:44:16 +0200

    Will it work?

--- oop.py
+++ oop.py
@@ -26,43 +26,84 @@
 # # d2 = Dog("Andrzej", 4)
 # # print(d2.get_age())
 
-class Student:
-    def __init__(self, name, age, grade):
+# class Student:
+#     def __init__(self, name, age, grade):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade #0 - 100
+    
+#     def get_grade(self):
+#         return self.grade
+    
+# class Course:
+#     def __init__(self, name, max_students):
+#         self.name = name
+#         self.max_students = max_students
+#         self.students = []
+
+#     def add_student(self, student):
+#         if len(self.students) < self.max_students:
+#             self.students.append(student)
+#             return True
+#         return False
+    
+#     def get_average_grade(self):
+#         average_grade = 0
+#         for student in self.students:
+#             average_grade += student.get_grade()
+        
+#         return average_grade / len(self.students)
+
+# s1 = Student("Cwel", 12, 95)
+# s2 = Student("Debil", 18, 14)
+# s3 = Student("JebanyDebil", 23, 30)
+# s4 = Student("Kutacz", 21, 55)
+
+# course = Course("Biologia", 3)
+# course.add_student(s1)
+# course.add_student(s2)
+# course.add_student(s3)
+
+# print(course.add_student(s4))
+# print(course.get_average_grade())
+class Pet:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grade = grade #0 - 100
-    
-    def get_grade(self):
-        return self.grade
-    
-class Course:
-    def __init__(self, name, max_students):
-        self.name = name
-        self.max_students = max_students
-        self.students = []
 
-    def add_student(self, student):
-        if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
-        return False
+    def show(self):
+        print(f"I am {self.name} my age is {self.age} years old")
+
+    def speak(self):
+        print("jestem nie binarny")
+
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
     
-    def get_average_grade(self):
-        average_grade = 0
-        for student in self.students:
-            average_grade += student.get_grade()
+    def show(self):
+        print(f"I am {self.name} my age is {self.age} years old and i am {self.color}")
         
-        return average_grade / len(self.students)
+    def speak(self):
+        print("Meow")
 
-s1 = Student("Cwel", 12, 95)
-s2 = Student("Debil", 18, 14)
-s3 = Student("JebanyDebil", 23, 30)
-s4 = Student("Kutacz", 21, 55)
+class Dog(Pet):
+    # def __init__(self, name, age):
+    #     self.name = name
+    #     self.age = age
+    
+    def speak(self):
+        print("Bark")
 
-course = Course("Biologia", 3)
-course.add_student(s1)
-course.add_student(s2)
-course.add_student(s3)
-course.add_student(s4)
+p = Pet("Retard", 14)
+p.show()
+p.speak()
 
-print(course.get_average_grade)
+c = Cat("JebanaKurwa", 5, "blue")
+c.show()
+c.speak()
+
+d = Dog("GlupiChuj", 12)
+d.show()
+d.speak()
