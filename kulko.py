11283ace5691dfa784898c1b17fe3e79026cc239
unknown <patrykszuraa@gmail.com>
2025-06-06 05:02:50 +0200

    getting closer

--- kulko.py
+++ kulko.py
@@ -1,5 +1,5 @@
 import random
-#create 3d arry for a grid and prints board 
+
 class Board:
     def __init__(self):
         self.board = [[" " for _ in range(3)] for _ in range(3)]
@@ -9,28 +9,37 @@ class Board:
             print(" | ".join(row))
             if i < 2:
                 print("--+---+--")
-game = Board()
+    def win_conditions(self):
+        pass
+
+board = Board()
 
 def computer_move(symbol):
-    return "computer"
+    print(symbol)
+
 def player_move(symbol):
-    return "plyer"
+    print(symbol)
+
+def is_input_valid(player_symbol):
+    while player_symbol not in ("X", "O"):
+        player_symbol = input("Chose symbol ('X' or 'O')").upper()
+    return player_symbol
 
 def main():
-    game.print_board()
+    board.print_board()
 
-    player_symbol = input("Chose symbol ('X' or 'O')").upper()
+    player_symbol = is_input_valid(input("Chose symbol ('X' or 'O')").upper())
+    computer_symbol = "O" if player_symbol == "X" else "X"
+
+    turn = "player" if player_symbol == "X" else "computer"
 
     while True:
-        while player_symbol not in ("X", "O"):
-            player_symbol = input("Chose symbol ('X' or 'O')").upper()
-
-        turn = "player" if player_symbol == "X" else "computer"
 
         if turn == "player":
-            print(player_move(player_symbol))
-            
+            player_move(player_symbol)
+            computer_move(computer_symbol)
         else:
-            print(computer_move(player_symbol))
-            
+            computer_move(computer_symbol)
+            player_move(player_symbol)
+
 main()
