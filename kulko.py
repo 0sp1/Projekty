fd3fc6b4cd008c72358e987cfe6f7a874a5da920
unknown <patrykszuraa@gmail.comm>
2025-06-09 12:15:42 +0200

    I M SUFFERING

--- kulko.py
+++ kulko.py
@@ -62,6 +62,7 @@ def main():
     computer_symbol = "O" if player_symbol == "X" else "X"
 
     turn = "player" if player_symbol == "X" else "computer"
+    os.system("cls")
     while True:
         if turn == "player":
             player_move(player_symbol, board)
@@ -82,3 +83,31 @@ def main():
         
         turn = "computer" if turn == "player" else "player"
 main()
+
+
+# Example file showing a basic pygame "game loop"
+import pygame
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
