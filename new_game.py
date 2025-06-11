ddbf2bbf4eeb1d0c200216a56e4f5f26fac94879
unknown <patrykszuraa@gmail.com>
2025-06-11 14:38:38 +0200

    AAAAAAAAAAAAAA IDE SPAĆ

--- new_game.py
+++ new_game.py
@@ -2,6 +2,7 @@ import pygame
 # pygame setup
 pygame.init()
 WIDTH, HEIGHT = 900, 900
+margin = 100
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
@@ -12,6 +13,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if pygame.mouse.get_pressed(num_buttons=3):
+        print("AAAAAAAAAAAA!!!!!")
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("grey")
@@ -22,7 +25,7 @@ while running:
     pygame.draw.line(screen, "black", (150, 350), (750, 350), width= 3)
     pygame.draw.line(screen, "black", (150, 550), (750, 550), width= 3)
 
-
+    
 
 
     # flip() the display to put your work on screen
