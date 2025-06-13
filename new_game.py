a03cfdd8ef97f373d7c9045b1b621fe3827c1515
unknown <patrykszuraa@gmail.comm>
2025-06-13 20:06:36 +0200

    UHHH

--- new_game.py
+++ new_game.py
@@ -4,30 +4,46 @@ class GameBoard():
     def __init__(self):
         self.board = [[" " for _ in range(3)] for _ in range(3)]
 
+    def draw_grid(self, screen, cell_size):
+        for i in range(1,3):
+            #horizontal lines
+            x = cell_size *i
+            pygame.draw.line(screen, "black", (0,x), (600, x))
+            #vertical lines 
+            y = x
+            pygame.draw.line(screen, "black", (y, 0), (y, 600))
+    def draw_O(self):
+        pass
+    def draw_X(self):
+        pass
+
 # pygame setup
 pygame.init()
-WIDTH, HEIGHT = 900, 900
-margin = 100
+WIDTH, HEIGHT = 600, 600
+cell_size = 200
+game = GameBoard()
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
-
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if pygame.mouse.get_pressed()[0]:
-        print(pygame.mouse.get_pos())
+    
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("grey")
 
     # RENDER YOUR GAME HERE
-    pygame.draw.line(screen, "black", (350, 150), (350,750), width = 3)
-    pygame.draw.line(screen, "black", (550, 150), (550,750), width = 3)
-    pygame.draw.line(screen, "black", (150, 350), (750, 350), width= 3)
-    pygame.draw.line(screen, "black", (150, 550), (750, 550), width= 3)
+    if pygame.mouse.get_pressed()[0]:
+        col, row =  pygame.mouse.get_pos()
+        col = col//cell_size
+        row = row//cell_size
+        print(col, row)
+
+    game.draw_grid(screen, cell_size)
 
     
     # flip() the display to put your work on screen
