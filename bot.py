fc08d18ee04451cbca3a7bad409d6f1c255d5621
unknown <patrykszuraa@gmail.comm>
2025-07-04 23:02:45 +0200

    michuj leniwy chuj

--- bot.py
+++ bot.py
@@ -1,27 +1,25 @@
 import discord
 from discord.ext import commands
+import yt_dlp
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-bot = commands.Bot(command_prefix="$", intents=intents)
-
+@bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
 @bot.command()
-async def hello(ctx, arg):
-    await ctx.send(f"What the fuck {arg}")
-
-@bot.command()
-async def join(ctx, arg):
+async def join(ctx):
     if ctx.author.voice:
         channel = ctx.author.voice.channel
         await channel.connect()
-        await ctx.send(f"Joined `{channel}`!{arg}")
+        await ctx.send(f"Joined `{channel}`!")
     else:
-        await ctx.send("You must be in a voice channel first.")
-        
+        await ctx.send("You're not in a voice channel.")
+
 @bot.command()
 async def play(ctx, url):
     if not ctx.author.voice:
@@ -34,6 +32,28 @@ async def play(ctx, url):
         voice_client = await channel.connect()
 
     # Download audio
+    await ctx.send("Downloading audio...")
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'song.%(ext)s',
+        'quiet': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info).replace('.webm', '.mp3').replace('.m4a', '.mp3')
+
+    # Play audio
+    source = discord.FFmpegPCMAudio(filename)
+    if voice_client.is_playing():
+        voice_client.stop()
+    voice_client.play(source, after=lambda e: print("Playback finished"))
+    await ctx.send(f"Now playing: **{info['title']}**")
 
 @bot.command()
 async def leave(ctx):
